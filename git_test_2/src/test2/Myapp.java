e08f771011adfc4669ed9e94a788fdbb7b8b2ae4
lamya <lamya@DESKTOP-PN9SI23>
2021-06-18 19:23:28 -0400

    first commit in dev

--- git_test_2/src/test2/Myapp.java
+++ git_test_2/src/test2/Myapp.java
@@ -3,8 +3,8 @@ package test2;
 public class Myapp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("first implementation in dev");
 	}
 
 }
