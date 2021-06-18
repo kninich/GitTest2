a24054afbaf1dfd0321713101a5d8af81b6f7ea2
lamya <lamya@DESKTOP-PN9SI23>
2021-06-18 19:28:31 -0400

    fifth commit in DEC 2

--- git_test_2/src/test2/Myapp.java
+++ git_test_2/src/test2/Myapp.java
@@ -3,8 +3,7 @@ package test2;
 public class Myapp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("fifth implementation in dev");
 	}
 
 }
