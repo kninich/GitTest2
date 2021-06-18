65b11e702fbde2a3b809423d5d556d9a6a59ae6b
lamya <lamya@DESKTOP-PN9SI23>
2021-06-18 19:24:35 -0400

    second commit in dev

--- git_test_2/src/test2/Myapp.java
+++ git_test_2/src/test2/Myapp.java
@@ -5,6 +5,7 @@ public class Myapp {
 	public static void main(String[] args) {
 
 		System.out.println("first implementation in dev");
+		System.out.println("second implementation in dev");
 	}
 
 }
